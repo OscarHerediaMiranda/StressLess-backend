6b45bd041951e4b4787c34dbebbeb9f04ba22ad9
OscarHerediaMiranda <u201913874@upc.edu.pe>
2025-05-18 02:20:56 -0500

    implementacion de tokens

--- auth.py
+++ auth.py
@@ -1,21 +1,31 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
-from models import Lider
+from models import Lider, Colaborador
 from database import get_session
 from pydantic import BaseModel
+
 
 router = APIRouter()
 
 class LoginRequest(BaseModel):
     correo: str
     contrasenia: str
+    rol: str
 
 @router.post("/login")
 def login(data: LoginRequest, session: Session = Depends(get_session)):
-    consulta = select(Lider).where(Lider.correo == data.correo)
-    resultado = session.exec(consulta).first()
+
+    if data.rol == "LIDER":
+        consulta = select(Lider).where(Lider.correo == data.correo)
+        resultado = session.exec(consulta).first()
+
+    if data.rol == "COLABORADOR":
+        consulta = select(Colaborador).where(Colaborador.correo == data.correo)
+        resultado = session.exec(consulta).first()
 
     if not resultado or resultado.contrasenia != data.contrasenia:
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
     
-    return {"mensaje": f"Bienvenido {resultado.nombre}"}
+    
+
+    return 
--- models.py
+++ models.py
@@ -10,3 +10,11 @@ class Lider(SQLModel, table=True):
     nombre_lider: str
     contrasenia: str
 
+class Colaborador(SQLModel, table=True):
+    _tablename_ = "colaborador"  # nombre de la tabla en minúsculas
+    _table_args_ = {"schema": "public"}  # esquema en PostgreSQL
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    correo: str
+    nombre_lider: str
+    contrasenia: str
